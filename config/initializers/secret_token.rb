41b4503453797290fdf51370ad6bba9f03db0252
Christina Thompson <c.thompsonms@gmail.com>
2014-03-31 14:35:54 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Colaboratory::Application.config.secret_key_base = '333f91574b7218c731ee4316383bf2356d1cec964d50beb0002cc38111f49d1988796b0e5e3029946486f53dfea1bc23549293b96de74cca8fbd55af6b4bc7ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Colaboratory::Application.config.session_store :cookie_store, key: '_colaboratory_session'
