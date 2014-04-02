e4dfcc937ab557ead0b77bf5485ab1993d8b5d5d
Christina Thompson <c.thompsonms@gmail.com>
2014-04-01 20:36:57 -0700

    create route for soundcloud oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Colaboratory::Application.routes.draw do
+
+  get "/auth/:provider", to: "sessions#create"
+  get "/auth/:provider/callback", to: "sessions#create" 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
