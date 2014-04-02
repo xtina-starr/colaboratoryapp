1015c119d901bab0c7f64ee816d26c7da6b9ba71
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-04-02 09:39:40 -0700

    Key updated

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Colaboratory::Application.config.secret_key_base = '333f91574b7218c731ee4316383bf2356d1cec964d50beb0002cc38111f49d1988796b0e5e3029946486f53dfea1bc23549293b96de74cca8fbd55af6b4bc7ee'
+Colaboratory::Application.config.secret_key_base = ENV['SECRET_KEY']
