8d92bfaa7d3c9b78fa0490a9c7f8f2d5cb510388
Christina Thompson <c.thompsonms@gmail.com>
2014-04-03 11:19:11 -0700

    create user with omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :providers
+  has_many :contents
+
+  def self.create_from_omniauth(auth_hasah)
+    self.create!(
+      username: auth_auth['extra']['username']
+      )
+  end
 end
