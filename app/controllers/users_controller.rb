638de33f53557c92f93b6054dbf7beda7a23f0b0
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-04-08 14:29:53 -0700

    Google oauth added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    # @client = VimeoProvider.get_client(@user)
-    @clientv = Vimeo::Advanced::Video.new(ENV["VIMEO_KEY"], ENV["VIMEO_SECRET"],
-      :token => current_user.providers.find_by(provider_type: "vimeo").token,
-      :secret => current_user.providers.find_by(provider_type: "vimeo").secret)
-    @videos = @clientv.get_all("colaboratory")
+    case
+    when @user.providers.where(provider_type: "vimeo").first
+      @clientv = Vimeo::Advanced::Video.new(ENV["VIMEO_KEY"], ENV["VIMEO_SECRET"],
+        :token => current_user.providers.find_by(provider_type: "vimeo").token,
+        :secret => current_user.providers.find_by(provider_type: "vimeo").secret)
+      @videos = @clientv.get_all("colaboratory")
 
-
-    @client = SoundcloudProvider.get_client(@user)
-    @tracks = @client.get('/me/tracks')
+    when @user.providers.where(provider_type: "soundcloud").first
+      @client = SoundcloudProvider.get_client(@user)
+      @tracks = @client.get('/me/tracks')
+    end
   end
 
   def update
