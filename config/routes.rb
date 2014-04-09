f1d87db137f17a4a66c4ec9278b5827103d90b04
Christina Thompson <c.thompsonms@gmail.com>
2014-04-09 16:52:17 -0700

    fix google oauth

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Colaboratory::Application.routes.draw do
 
   get "welcome/index"
 
-  get "/auth/:provider",          to: "sessions#create"
+  # get "/auth/:provider",          to: "sessions#create"
   get "/auth/:provider/callback", to: "sessions#create"
 
   get "/signout",                 to: "sessions#signout", as: :sign_out
