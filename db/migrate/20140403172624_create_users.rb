bbb0d0d5e5827abc4187fe23744bfd2afc6d13ce
Christina Thompson <c.thompsonms@gmail.com>
2014-04-11 14:13:16 -0700

    add roles to user table

--- db/migrate/20140403172624_create_users.rb
+++ db/migrate/20140403172624_create_users.rb
@@ -1,9 +1,22 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :username
-      t.string :email
+      t.string  :name
+      t.string  :username
+      t.string  :email
+      t.string  :location
+      t.string  :website
+      t.string  :bio
+      t.boolean :dj, :default => false
+      t.boolean :producer, :default => false 
+      t.boolean :rapper, :default => false
+      t.boolean :singer, :default => false
+      t.boolean :songwriter, :default => false
+      t.boolean :musician, :default => false
+      t.boolean :animator, :default => false
+      t.boolean :filmmaker, :default => false
+      t.boolean :videographer, :default => false
+      t.boolean :editor, :default => false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,19 @@ ActiveRecord::Schema.define(version: 20140403172916) do
     t.string   "name"
     t.string   "username"
     t.string   "email"
+    t.string   "location"
+    t.string   "website"
+    t.string   "bio"
+    t.boolean  "dj",           default: false
+    t.boolean  "producer",     default: false
+    t.boolean  "rapper",       default: false
+    t.boolean  "singer",       default: false
+    t.boolean  "songwriter",   default: false
+    t.boolean  "musician",     default: false
+    t.boolean  "animator",     default: false
+    t.boolean  "filmmaker",    default: false
+    t.boolean  "videographer", default: false
+    t.boolean  "editor",       default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
