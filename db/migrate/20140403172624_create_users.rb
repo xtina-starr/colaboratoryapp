11c20198d8573a28a510b2092401bbe1bc3933b7
Christina Thompson <c.thompsonms@gmail.com>
2014-04-11 14:15:04 -0700

    style

--- db/migrate/20140403172624_create_users.rb
+++ db/migrate/20140403172624_create_users.rb
@@ -7,16 +7,16 @@ class CreateUsers < ActiveRecord::Migration
       t.string  :location
       t.string  :website
       t.string  :bio
-      t.boolean :dj, :default => false
-      t.boolean :producer, :default => false 
-      t.boolean :rapper, :default => false
-      t.boolean :singer, :default => false
-      t.boolean :songwriter, :default => false
-      t.boolean :musician, :default => false
-      t.boolean :animator, :default => false
-      t.boolean :filmmaker, :default => false
+      t.boolean :dj,           :default => false
+      t.boolean :producer,     :default => false 
+      t.boolean :rapper,       :default => false
+      t.boolean :singer,       :default => false
+      t.boolean :songwriter,   :default => false
+      t.boolean :musician,     :default => false
+      t.boolean :animator,     :default => false
+      t.boolean :filmmaker,    :default => false
       t.boolean :videographer, :default => false
-      t.boolean :editor, :default => false
+      t.boolean :editor,       :default => false
 
       t.timestamps
     end
