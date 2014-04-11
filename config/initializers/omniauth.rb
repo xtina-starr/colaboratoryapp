d16e053052b429938b8535e8dd5ce18fe91aed85
Christina Thompson <c.thompsonms@gmail.com>
2014-04-11 14:16:10 -0700

    got a cross site forgery request

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'],
     {
     #   # :name => "google",
+      :provider_ignores_state => true,
       :scope => "userinfo.email, userinfo.profile, plus.me, http://gdata.youtube.com",
       :prompt => "select_account",
       :image_aspect_ratio => "square",
