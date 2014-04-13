88bfc07741e42b686b73a9201a27d67b99e6a85f
Christina Thompson <c.thompsonms@gmail.com>
2014-04-12 17:00:37 -0700

    roles helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,32 @@ module ApplicationHelper
     if user.dj
       "DJ"
     end
+    if user.producer
+      "producer"
+    end
+    if user.rapper
+      "rapper"
+    end
+    if user.singer
+      "singer"
+    end
+    if user.songwriter
+      "songwriter"
+    end
+    if user.musician
+      "musician"
+    end
+    if user.animator
+      "animator"
+    end
+    if user.filmmaker
+      "filmmaker"
+    end
+    if user.videographer
+      "videographer"
+    end
+    if user.editor
+      "editor"
+    end
   end
 end
