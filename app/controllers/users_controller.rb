7dd347ee14f6d067b1155b786246127f8805a189
Christina Thompson <c.thompsonms@gmail.com>
2014-04-12 21:52:14 -0700

    changed login flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,30 @@ class UsersController < ApplicationController
   def index
   end
 
+  def user_profile
+    @user = User.find(params[:id])
+
+    if @user.providers.where(provider_type: "vimeo").first
+      @clientv = Vimeo::Advanced::Video.new(ENV["VIMEO_KEY"], ENV["VIMEO_SECRET"],
+        :token => current_user.providers.find_by(provider_type: "vimeo").token,
+        :secret => current_user.providers.find_by(provider_type: "vimeo").secret)
+      @videos = @clientv.get_all("colaboratory")
+    end
+
+    if @user.providers.where(provider_type: "soundcloud").first
+      @client = SoundcloudProvider.get_client(@user)
+      @tracks = @client.get('/me/tracks')
+    end
+
+    if @user.providers.where(provider_type: "google_oauth2").first
+
+      user_token = @user.providers.where(provider_type: "google_oauth2").first.token
+
+      @youtubes = YoutubeProvider.new.get_videos_for(user_token)
+    end
+
+  end
+
   def show
     @user = User.find(params[:id])
 
@@ -49,7 +73,7 @@ class UsersController < ApplicationController
       videographer: params[:videographer],
       animator: params[:editor])
 
-      redirect_to user_path(@user.id), notice: 'User was successfully updated.'
+      redirect_to user_profile_path(@user.id), notice: 'User was successfully updated.'
     else
       render action: 'edit'
     end
