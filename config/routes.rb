e7a2202eda8412613db389ea17730933e6f0c44c
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-04-14 13:55:04 -0700

    Learn More page modals added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Colaboratory::Application.routes.draw do
 
-  get "static_pages/help",       to: "static_pages#help",       as: :help
-  get "static_pages/about",      to: "static_pages#about_us",   as: :about
-  get "static_pages/learn_more", to: "static_pages#learn_more", as: :learn_more
-  get "static_pages/signup",     to: "static_pages#signup",     as: :signup
+  get "/help",       to: "static_pages#help",       as: :help
+  get "/about",      to: "static_pages#about_us",   as: :about
+  get "/learn_more", to: "static_pages#learn_more", as: :learn_more
+  get "/signup",     to: "static_pages#signup",     as: :signup
   root 'welcome#index'
   get "users/index"
   get "users/show"
