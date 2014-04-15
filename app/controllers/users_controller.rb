e340234d5e3c02baca423d668dae0722017eee3c
Christina Thompson <c.thompsonms@gmail.com>
2014-04-15 10:39:02 -0700

    no longer make api call for profile. Hit db.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,25 +28,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-
-    if @user.providers.where(provider_type: "vimeo").first
-      @clientv = Vimeo::Advanced::Video.new(ENV["VIMEO_KEY"], ENV["VIMEO_SECRET"],
-        :token => current_user.providers.find_by(provider_type: "vimeo").token,
-        :secret => current_user.providers.find_by(provider_type: "vimeo").secret)
-      @videos = @clientv.get_all("colaboratory")
-    end
-
-    if @user.providers.where(provider_type: "soundcloud").first
-      @client = SoundcloudProvider.get_client(@user)
-      @tracks = @client.get('/me/tracks')
-    end
-
-    if @user.providers.where(provider_type: "google_oauth2").first
-
-      user_token = @user.providers.where(provider_type: "google_oauth2").first.token
-
-      # @youtubes = YoutubeProvider.new.get_videos_for(user_token)
-    end
+    @contents = @user.contents
   end
 
   def edit
