e4d1995d62ddbfeacca10a8beb15779119f730b5
Christina Thompson <c.thompsonms@gmail.com>
2014-04-15 12:04:59 -0700

    delete commented out code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -33,12 +33,6 @@ class SessionsController < ApplicationController
      session[:user] = nil
      redirect_to "/", notice: "You have been successfully signed out."
    end
-    # Vimeo
-    # video = Vimeo::Advanced::Video.new(ENV["VIMEO_KEY"], ENV["VIMEO_SECRET"], token: auth_hash.extra.access_token.token, secret: auth_hash.extra.access_token.secret)
-    # raise
-
-    # video_embed = Vimeo::Advanced::Video.new(ENV["VIMEO_KEY"], ENV["VIMEO_SECRET"], token: auth_hash.extra.access_token.token, secret: auth_hash.extra.access_token.secret)
-    # video_embed.get_presets({ page: "1", per_page: "10" })
 
 
   def show
