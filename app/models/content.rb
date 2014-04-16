8a4aefa1f909278cc2372f7f2e37379bd9d83b5a
Christina Thompson <c.thompsonms@gmail.com>
2014-04-16 14:58:53 -0700

    search

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,7 +3,7 @@ class Content < ActiveRecord::Base
   belongs_to :provider
 
   # def search(search_term)
-  #   User.includes(:content).where("username LIKE :query OR name LIKE :query OR bio LIKE :query OR media_type LIKE :query", query: %search_term% )
+  #   User.includes(:content).where("username LIKE :query OR name LIKE :query OR bio LIKE :query OR media_type LIKE :query", query: "%search_term%" )
   # end
 
 end
