37479608892c0124ac0f7f97ddf901e093b4e37b
Christina Thompson <c.thompsonms@gmail.com>
2014-04-16 09:25:10 -0700

    start on search

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,8 @@
 class Content < ActiveRecord::Base
   belongs_to :user
   belongs_to :provider
+
+  # def search(search_term)
+  #   User.where("username LIKE ? OR name LIKE ?", search_term, search_term)
+  # end
 end
