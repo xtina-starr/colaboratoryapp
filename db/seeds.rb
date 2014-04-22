148b19cefa6d1552c12fba86e3cc7139f9bdecf9
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-04-22 15:34:37 -0700

    Fixed background container

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@
 # repeat for each user
 user = User.create(name: "Beyonce", username: "QueenB", email: "Bey@Bey.com", location: "The World, USA", website: "http://www.beyonce.com/", bio: "Running the world, one step at a time!", producer: true, rapper: true, singer: true, songwriter: true)
 Provider.create(provider_type: "vimeo", username: "Beyonce", user_id: user.id)
-user.contents.create(user_id: user.id, provider_id: 1, feedback: true, media_id: "24208562", title: "Run the World")
-user.contents.create(user_id: user.id, provider_id: 1, feedback: true, media_id: "82541445", title: "Beyonce 'Yonce'")
+user.contents.create(user_id: 1, provider_id: 1, feedback: true, media_id: "24208562", title: "Run the World")
+user.contents.create(user_id: 1, provider_id: 1, feedback: true, media_id: "82541445", title: "Beyonce 'Yonce'")
 
 user2 = User.create(name: "Matthew Intreg", username: "Matthew Intreg", email: "Intreg@Intreg.com", location: "Seattle, WA", website: "http://intregemarketing.com/", bio: "I love making illustrations. Let me know what you think?", animator: true, videographer: true, editor: true)
 Provider.create(provider_type: "vimeo", username: "Matthew Intreg", user_id: user2.id)
-user.contents.create(user_id: user2.id, provider_id: 2, feedback: true, collab: true, media_id: "92301646", title: "Rainbow")
-user.contents.create(user_id: user2.id, provider_id: 2, feedback: true, collab: true, media_id: "92224516", title: "The Lava")
+user2.contents.create(user_id: user2.id, provider_id: 1, feedback: true, collab: true, media_id: "92301646", title: "Rainbow")
+user2.contents.create(user_id: user2.id, provider_id: 1, feedback: true, collab: true, media_id: "92224516", title: "The Lava")
